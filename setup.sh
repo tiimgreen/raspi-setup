8c33d1a22d09c564ff23563147f6b69eb690275c
Tim Green <tiimgreen@gmail.com>
2018-06-15 18:17:24 +0100

    add git configuration

--- setup.sh
+++ setup.sh
@@ -6,7 +6,10 @@ cd ~/node-sonos-http-api
 npm install --production
 cd ~
 bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)"
+git config --global user.email "tiimgreen@gmail.com"
+git config --global user.name "Tim Green"
 cd MagicMirror
+git stash
 git remote set-url origin https://github.com/tiimgreen/MagicMirror
 git pull
 cd modules
