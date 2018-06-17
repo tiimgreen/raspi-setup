48c43c8c6753daef9f6934f8a31d14399fe0d837
Tim Green <tiimgreen@gmail.com>
2018-06-17 17:25:27 +0100

    add comments, update for MTI

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
+# updates
 sudo apt-get update
 sudo apt-get upgrade
+
+# node-sonos-http-api
 sudo apt-get install nodejs npm
 git clone https://github.com/jishi/node-sonos-http-api ~/node-sonos-http-api
 cd ~/node-sonos-http-api
 npm install --production
+
+# MagicMirror
 cd ~
 bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)"
 git config --global user.email "tiimgreen@gmail.com"
@@ -16,13 +21,17 @@ cd modules
 git clone https://github.com/tiimgreen/MMM-Football-Fixtures
 git clone https://github.com/tiimgreen/MMM-cryptocurrency
 git clone https://github.com/tiimgreen/MMM-Sonos-Control
+
+# skywriter
 cd ~
 curl -sS get.pimoroni.com/skywriter | bash
-git clone https://github.com/tiimgreen/skywriter.py
-touch skywriter.sh
-chmod 777 skywriter.sh
-echo "cd skywriter.py" >> skywriter.sh
-echo "python3 skywriter.py" >> skywriter.sh
+git clone https://github.com/tiimgreen/mti
+mv ~/mti/mti.py ~/Pimoroni/skywriter/examples/mti.py
+
+# create pm2 files
+touch mti.sh
+chmod 777 mti.sh
+echo "python3 ~/Pimoroni/skywriter/examples/mti.py" >> mti.sh
 touch magic_mirror.sh
 chmod 777 magic_mirror.sh
 echo "cd MagicMirror" >> magic_mirror.sh
@@ -31,3 +40,9 @@ touch node_sonos.sh
 chmod 777 node_sonos.sh
 echo "cd node-sonos-http-api" >> node_sonos.sh
 echo "npm start" >> node_sonos.sh
+
+# start pm2
+pm2 start mti.sh
+pm2 start magic_mirror.sh
+pm2 start node_sonos.sh
+pm2 save
